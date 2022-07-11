921e3e16898f6ce9d10eb408250aa223807983cd
Dima Eph <thelegodorkdima@gmail.com>
2022-07-12 02:14:45 +0300

    First commit

---
+++ src/Parser.java
@@ -0,0 +1,83 @@
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Parser {
+    public final String URL_CRYPTO = "https://myfin.by/crypto-rates";
+    public String title;
+    private String Url = "";
+    private Document document;
+    public void LoadDocument(String url)
+    {
+        if(url.isEmpty())
+        {
+            System.out.printf("Введенная ссылка пуста!\n");
+            return;
+        }
+        try {
+            document = Jsoup.connect(url).get();
+            Element titleElement = document.selectFirst("title");
+            title = titleElement.text();
+            System.out.printf("Страница успешно загружена!\tЕё заголовок - <%s>\n",titleElement.text());
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+    public void GetRefs()
+    {
+        if(document == null)
+        {
+            System.out.printf("Вы не загрузили страницу!\n");
+            return;
+        }
+
+        Elements refsEl = document.select("a");
+        String[] refsAll = new String[refsEl.size()];
+        ArrayList<String> refsList = new ArrayList<String>();
+
+        for (int i = 0; i < refsEl.size(); i++)
+            refsAll[i] = refsEl.get(i).attr("href");
+
+        for (int i = 0; i < refsAll.length; i++)
+            if(refsAll[i].contains("https://"))
+                refsList.add(refsAll[i]);
+
+        String[] refs = new String[refsList.size()];
+        for (int i = 0; i < refsList.size(); i++)
+            refs[i] = refsList.get(i);
+
+
+        System.out.printf("Ссылки страницы с заголовком - <%s>:\n", title);
+        for (int i = 0; i < refs.length; i++)
+            System.out.printf("%d.%s\n",i,refs[i]);
+    }
+    public void GetPricesCrypto()
+    {
+        Document doc = null;
+        try {
+            doc = Jsoup.connect(URL_CRYPTO).get();
+        } catch (Exception e) {
+            System.out.printf("Произошла непредвиденная ошибка при загрузке страницы:\n%s\n",e.getMessage());
+        }
+        Elements tdElements = doc.select("td");
+        Elements refElements = doc.select("a");
+        ArrayList<String> prices = new ArrayList<String>();
+        ArrayList<String> names = new ArrayList<String>();
+
+        for (int i = 0; i < tdElements.size(); i++)
+            if(tdElements.get(i).text().contains("$") & tdElements.get(i).className() == "")
+                prices.add(tdElements.get(i).text().split(" ")[0]);
+        System.out.println(refElements.size());
+        for (int i = 0; i < refElements.size(); i++)
+            if(refElements.get(i).hasClass("s-bold"))
+                names.add(refElements.get(i).text());
+
+        System.out.printf("Актуальные цены криптовалют с сайта - <%s>:\n",URL_CRYPTO);
+        for (int i = 0; i < prices.size(); i++)
+            System.out.printf("%s  -  %s\n",names.get(i),prices.get(i));
+    }
+}
---
+++ src/Program.java
@@ -0,0 +1,41 @@
+import java.util.Scanner;
+
+public class Program {
+    public static void main(String [] args)
+    {
+        Scanner scan = new Scanner(System.in);
+        Parser parser = new Parser();
+        while (true)
+        {
+            System.out.printf("Выберите действие:\n<1> - Загрузить страницу.\n<2> - Получить ссылки страницы.\n" +
+                    "<3> - Получить актуальные цены криптовалют.\n");
+            String choise = scan.next();
+            switch (choise)
+            {
+                case "1":
+                {
+                    System.out.printf("Введите ссылку страницы:\n");
+                    String url = scan.next();
+                    parser.LoadDocument(url);
+                    break;
+                }
+                case "2":
+                {
+                    parser.GetRefs();
+                    break;
+                }
+                case "3":
+                {
+                    parser.GetPricesCrypto();
+                    break;
+                }
+                default:
+                {
+                    System.out.printf("Данного действия не существует!\n");
+                    break;
+                }
+            }
+
+        }
+    }
+}
